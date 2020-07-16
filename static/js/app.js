bc00094c62b7a2f3ca12e15fcae4e34740837dde
Veronica Ratcliff <vmratcliff@Veronicas-MBP-2.lan>
2020-07-16 09:47:40 -0500

    added two more pue charts

--- static/js/app.js
+++ static/js/app.js
@@ -53,6 +53,105 @@ function init() {
 
 
         Plotly.newPlot("pie", [pieTrace], pieLayout);
+    
+        // Pie chart for Employment Type
+        var emp_type = filteredData.map(d => d.employment_type)
+
+        var filtered_empT= emp_type.filter(d => d !== "")
+        console.log(filtered_empT)
+            // var x = category_count.filter(d => d.value === jobcat)
+        var i = 0
+        if (jobcat === "Real") {
+            i = 0
+        };
+
+        // console.log(x);
+        var pieEmp = {
+            values: category_count[i].values,
+            labels: filtered_empT,
+            // hovertext: count + labels,
+            hoverinfo: 'hovertext',
+            type: 'pie',
+
+        };
+
+        var pieLayout = {
+            'legend': {
+                x: 0.1,
+                // y: 5,
+                'orientation': 'h'
+            }
+        };
+        // title: `Pie Chart `,
+
+        Plotly.newPlot("empPie", [pieEmp], pieLayout);
+
+
+        // Pie chart for Required Experience
+        var exp_type = filteredData.map(d => d.required_experience)
+
+        var filtered_reqExp= exp_type.filter(d => d !== "")
+        console.log(filtered_reqExp)
+            // var x = category_count.filter(d => d.value === jobcat)
+        var i = 0
+        if (jobcat === "Real") {
+            i = 0
+        };
+
+        // console.log(x);
+        var pieReqexp = {
+            values: category_count[i].values,
+            labels: filtered_reqExp,
+            // hovertext: count + labels,
+            hoverinfo: 'hovertext',
+            type: 'pie',
+
+        };
+
+        var pieLayout = {
+            'legend': {
+                x: 0.1,
+                // y: 5,
+                'orientation': 'h'
+            }
+        };
+        // title: `Pie Chart `,
+
+        Plotly.newPlot("reqExpPie", [pieReqexp], pieLayout);
+
+
+        // Bar Chart Top 10 Job Titles
+        // var job_titles = filteredData.map(d => d.title)
+
+        // var filtered_title= job_titles.filter(d => d !== "")
+        // console.log(filtered_title
+        //     // var x = category_count.filter(d => d.value === jobcat)
+        // var i = 0
+        // if (jobcat === "Real") {
+        //     i = 0
+        // };
+
+        // // console.log(x);
+        // var pieReqexp = {
+        //     values: category_count[i].values,
+        //     labels: filtered_reqExp,
+        //     // hovertext: count + labels,
+        //     hoverinfo: 'hovertext',
+        //     type: 'pie',
+
+        // };
+
+        // var pieLayout = {
+        //     'legend': {
+        //         x: 0.1,
+        //         // y: 5,
+        //         'orientation': 'h'
+        //     }
+        // };
+
+        // Plotly.newPlot("reqExpPie", [pieReqexp], pieLayout);
+ 
+        
     }
 
     test();
