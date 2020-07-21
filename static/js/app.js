cf1489bf1f3d6f2bbdba6c93bb37f91c8223ece4
busy0312 <abbymacbook@Abbys-MacBook-Pro.local>
2020-07-21 00:49:03 -0500

    made pie chart responsive

--- static/js/app.js
+++ static/js/app.js
@@ -4,37 +4,30 @@ function init() {
         console.log(data);
 
         var jobcat = d3.select("#selvalue").node().value;
-        console.log(jobcat);
 
         var filteredData = data.filter(d => d.fraudulent === jobcat);
-        // console.log(filteredData);
-
 
         var category_count = d3.nest()
-            .key(function(d) {
+            .key(function (d) {
                 return d.fraudulent;
             })
-            .rollup(function(leaves) {
+            .rollup(function (leaves) {
                 return leaves.length;
             })
             .entries(data);
         // console.log(category_count)
-
-
         var req_ed = filteredData.map(d => d.required_education)
-
         var filtered_ed = req_ed.filter(d => d !== "")
-            // console.log(filtered_ed)
-            // var x = category_count.filter(d => d.value === jobcat)
+        // console.log(filtered_ed)
+        // var x = category_count.filter(d => d.value === jobcat)
         var i = 0
         if (jobcat === "Real") {
             i = 0
         };
         var color1 = [
-                "#154360", "#1A5276", "#1F618D", "#2471A3", "#2980B9", "#5499C7", "#7FB3D5",
-                "#A9CCE3", "#D4E6F1", "#EAF2F8"
-            ]
-            // console.log(x);
+            "#154360", "#1A5276", "#1F618D", "#2471A3", "#2980B9", "#5499C7", "#7FB3D5",
+            "#A9CCE3", "#D4E6F1", "#EAF2F8"
+        ]
         var pieTrace = {
             values: category_count[i].values,
             labels: filtered_ed,
@@ -66,23 +59,20 @@ function init() {
             },
 
         };
+        var config = { responsive: true }
         // title: `Pie Chart `,
-
-        Plotly.newPlot("pie", [pieTrace], pieLayout);
-
+        Plotly.newPlot("pie", [pieTrace], pieLayout, config);
 
         // Pie chart for Employment Type
         var emp_type = filteredData.map(d => d.employment_type)
 
         var filtered_empT = emp_type.filter(d => d !== "")
-            // console.log(filtered_empT)
-            // var x = category_count.filter(d => d.value === jobcat)
+        // console.log(filtered_empT)
+        // var x = category_count.filter(d => d.value === jobcat)
         var i = 0
         if (jobcat === "Real") {
             i = 0
         };
-
-        // console.log(x);
         var pieEmp = {
             values: category_count[i].values,
             labels: filtered_empT,
@@ -111,22 +101,18 @@ function init() {
             }
         };
         // title: `Pie Chart `,
-
-        Plotly.newPlot("empPie", [pieEmp], pieLayout);
-
+        Plotly.newPlot("empPie", [pieEmp], pieLayout, config);
 
         // Pie chart for Required Experience
         var exp_type = filteredData.map(d => d.required_experience)
-
         var filtered_reqExp = exp_type.filter(d => d !== "")
-            // console.log(filtered_reqExp)
-            // var x = category_count.filter(d => d.value === jobcat)
+        // console.log(filtered_reqExp)
+        // var x = category_count.filter(d => d.value === jobcat)
         var i = 0
         if (jobcat === "Real") {
             i = 0
         };
 
-        // console.log(x);
         var pieReqexp = {
             values: category_count[i].values,
             labels: filtered_reqExp,
@@ -156,12 +142,10 @@ function init() {
         };
         // title: `Pie Chart `,
 
-        Plotly.newPlot("reqExpPie", [pieReqexp], pieLayout);
-
+        Plotly.newPlot("reqExpPie", [pieReqexp], pieLayout, config);
 
         // Bar Chart Top 10 Job Titles
         var xjob_titles = filteredData.map(d => d.title)
-
         var title_counts = {}
         xjob_titles.forEach(d => {
             if (d in title_counts) {
@@ -170,26 +154,19 @@ function init() {
                 title_counts[d] = 1
             }
         })
-
-        console.log(title_counts)
-        var items = Object.keys(title_counts).map(function(key) {
+        var items = Object.keys(title_counts).map(function (key) {
             return [key, title_counts[key]];
         });
         // Sort the array based on the second element
-        items.sort(function(first, second) {
+        items.sort(function (first, second) {
             return second[1] - first[1];
         });
-
-        // console.log(items)
-
         sorted_items = items.slice(0, 10)
-            // console.log(sorted_items)
-
 
         // var fraudtype= filteredData.map(d => d.fraudulent)
         var filtered_title = xjob_titles.filter(d => d !== "")
         console.log(filtered_title)
-            // var x = category_count.filter(d => d.value === jobcat)
+        // var x = category_count.filter(d => d.value === jobcat)
         var i = 0
         if (jobcat === "Real") {
             i = 0
@@ -226,8 +203,6 @@ function init() {
         var data_bar = [trace];
 
         Plotly.newPlot("bar", data_bar, layout, config);
-
-
     }
 
     test();
